e928a47f808bbd15fd08dfc5611c6be28da1f7fe
Beshoy Abd Elsayed <beshoabdo8@gmail.com>
2021-01-06 16:40:44 +0200

    feat: create the navbar links dynamically

--- js/app.js
+++ js/app.js
@@ -25,6 +25,42 @@
  * 
 */
 
+/**
+ * get the sections in the page.
+ * @returns {Object}
+ */
+const getSections = () => {
+    return document.querySelectorAll('section');
+};
+
+/**
+ * create the nav bar links
+ * @param {Object} sections 
+ * @returns {Object}
+ */
+const createNavLinks = sections => {
+    const navLinks = document.createDocumentFragment();
+    for (section of sections) {
+        const listItem = document.createElement('li');
+        const anchor = document.createElement('a');
+        anchor.classList.add('menu__link');
+        anchor.href = '#' + section.id;
+        anchor.textContent = section.dataset.nav;
+        listItem.appendChild(anchor);
+        navLinks.appendChild(listItem);
+    }
+    return navLinks;
+};
+
+/**
+ * insert the nave links into the DOM
+ * @param {Object} navLinks 
+ */
+const insertNavLinks = navLinks => {
+    const navList = document.getElementById('navbar__list');
+    navList.appendChild(navLinks);
+    return;
+};
 
 
 /**
@@ -34,7 +70,8 @@
 */
 
 // build the nav
-
+const navlinks = createNavLinks(getSections());
+insertNavLinks(navlinks);
 
 // Add class 'active' to section when near top of viewport
 
